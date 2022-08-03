3891be133e995d261c470b1df2da26c689f50819
LeonardoGasperin <gasperinlvg@gmail.com>
2022-08-02 22:47:32 -0300

    adicionado mecanica de adicionar cartoes

--- site/js/card.js
+++ site/js/card.js
@@ -1,4 +1,4 @@
-export default class card{
+export default class Card{
     constructor(title, skill, cat, desc, hLink){
         this.title = title;
         this.skill = skill;
@@ -7,8 +7,54 @@ export default class card{
         this.hLink = hLink;
     }
 
-    makecard(){
-        ///TODO
-            //fazer a logica do HTML aqui
+    makecard(obj = new Card()){
+        const asideBx = document.getElementById("tipBoard"); 
+        const descAside = document.createElement("aside");
+        const asideDiv = document.createElement("div");
+        const cardTitle = document.createElement("h4");
+        const cardSkill = document.createElement("p");        
+        const cardCat = document.createElement("p");
+        const cardDesc = document.createElement("p");
+        //        const cardLink = document.createElement("??");
+        const divBtn = document.createElement("div");
+        const btnDel = document.createElement("button");
+        const btnEdit = document.createElement("button");;
+        const btnVideo = document.createElement("button");
+
+        const strongSkill = document.createElement("strong");
+        strongSkill.innerText = "Skill: ";
+        const strongCat = document.createElement("strong");
+        strongCat.innerText = "Categoria: ";
+        
+        asideDiv.className = "tip";
+        descAside.className = "tipAside";
+        cardSkill.className = "tipClass";
+        cardCat.className = "tipClass";
+        cardDesc.className = "desc";
+        divBtn.id = "tipbtn";
+        btnDel.className = "tipbtn";
+        btnDel.id = "del";
+        btnEdit.className = "tipbtn";
+        btnEdit.id = "edit";
+        btnVideo.className = "tipbtn";
+        btnVideo.id = "video";
+
+        cardTitle.innerHTML = obj.title;
+        asideDiv.append(cardTitle);
+        cardSkill.append(strongSkill);
+        cardSkill.innerHTML += obj.skill;
+        asideDiv.append(cardSkill);
+        cardCat.append(strongCat);
+        cardCat.innerHTML += obj.cat;
+        asideDiv.append(cardCat);
+        cardDesc.innerHTML = obj.desc;
+        descAside.append(cardDesc);
+        asideDiv.append(descAside);
+        divBtn.append(btnDel);
+        divBtn.append(btnEdit);
+        divBtn.append(btnVideo);
+        asideDiv.append(divBtn);
+        asideBx.appendChild(asideDiv);
+
     }
 }
--- site/js/module.js
+++ site/js/module.js
@@ -1,7 +1,55 @@
-import {  } from "./card.js";
+import Card, {  } from "./card.js";
 
 const btnSvCreation = document.getElementById("svCr");
+const title = document.getElementById("title");
+const skill = document.getElementById("skill");
+const category = document.getElementById("category");
+const desc = document.getElementById("desc");
+const video = document.getElementById("video");
+
+addEventListener("keydown", () => {
+    //console.log()//assert
+
+    if(title.value != "" && title.value.length >= 7 && title.value.charAt(7) != "" && title.value.length <= 15
+    && skill.value != "" && skill.value.length >= 2 /*&& skill.value.charAt(7) != ""*/ && title.value.length <= 15
+    && category.selectedIndex != 0
+    && (desc.value == "" || desc.value.length >= 16))
+        btnSvCreation.disabled = false;
+    else{
+        btnSvCreation.disabled = true;
+    }
+})
+
+addEventListener("click", () => {
+    //console.log()//assert
+    
+    if(title.value != "" && title.value.length >= 7 && title.value.charAt(7) != "" && title.value.length <= 63
+    && skill.value != "" && skill.value.length >= 2 /*&& skill.value.charAt(7) != ""*/ && title.value.length <= 15
+    && category.selectedIndex != 0
+    && (desc.value == "" || desc.value.length >= 16))
+        btnSvCreation.disabled = false;
+    else{
+        btnSvCreation.disabled = true;
+    }
+})
 
 btnSvCreation.addEventListener("click", () => {
+    if((desc.value == "" || desc.value.length >= 16)){
+        const card = new Card(
+            title.value,
+            skill.value,
+            category.options[category.selectedIndex].text,
+            desc.value,
+            video.value
+        )
 
+        card.makecard(card);
+    }
 })
+
+function chkLink(hLink){
+    if(hLink)
+        return hLink;
+    else
+        video.value = "";
+}
